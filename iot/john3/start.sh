e081864414b25b8e516f5cee749019b2cb0572fc
JohnSteck9 <m0nitor2048@gmail.com>
2021-12-09 18:21:09 +0200

    fix name files

--- iot/john3/start.sh
+++ iot/john3/start.sh
@@ -1,5 +1,5 @@
 while [ : ]
 do
-    python3 post.py --endpoint a26ctyxyqff3du-ats.iot.us-east-1.amazonaws.com --cert 81d0eea90f1ea2640051e6dcb022e5ab8e9ffac842b35ab688d88d5e2c84b877-certificate.pem.crt --key d4c3065d02c9de1542250d6006a0c60ae8266cf88179bcf550132b244c1c0e6b-private.pem.key --topic iot_sub
+    python3 post.py --endpoint a26ctyxyqff3du-ats.iot.us-east-1.amazonaws.com --cert d4c3065d02c9de1542250d6006a0c60ae8266cf88179bcf550132b244c1c0e6b-certificate.pem.crt --key d4c3065d02c9de1542250d6006a0c60ae8266cf88179bcf550132b244c1c0e6b-private.pem.key --topic iot_sub
     sleep 10
 done
