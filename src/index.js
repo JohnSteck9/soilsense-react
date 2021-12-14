0833b97183a76658160af747bb0054f5c96e8260
JohnSteck9 <m0nitor2048@gmail.com>
2021-12-15 01:06:16 +0200

    test css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.scss';
+import './style.css';
 // import {Amplify} from "aws-amplify";
 // import config from './aws-exports.js';
 
